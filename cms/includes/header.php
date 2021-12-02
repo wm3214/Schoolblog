acfc6d4691020b54156510370e75b5c4b599b722
Billy <billy.van.zaal@windesheim.nl>
2021-12-02 12:46:03 +0100

    CSS pathing verbeterd

--- cms/includes/header.php
+++ cms/includes/header.php
@@ -12,10 +12,10 @@
     <title>Schoolblog Wendy </title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/cms/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/cms/css/blog-home.css" rel="stylesheet">
+    <link href="/css/blog-home.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
